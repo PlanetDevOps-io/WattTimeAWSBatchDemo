d5eeb826cd4033204ace75298987886825d6e766
Edwin Radtke <e>
2023-03-27 15:46:25 +0200

    Initial commit

---
+++ test/wattimeawsbatch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Wattimeawsbatch from '../lib/wattimeawsbatch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wattimeawsbatch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Wattimeawsbatch.WattimeawsbatchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
